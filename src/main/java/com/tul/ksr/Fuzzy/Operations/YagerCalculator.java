eb21453afba1737a6ad2d97b3d29101ea1ffc4bf
madekko <madej222@wp.pl>
2018-06-02 18:42:30 +0200

    final version of Yager

--- src/main/java/com/tul/ksr/Fuzzy/Operations/YagerCalculator.java
+++ src/main/java/com/tul/ksr/Fuzzy/Operations/YagerCalculator.java
@@ -9,35 +9,44 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class YagerCalculator {
-    Quantifier quantifier;
-    LinguisticValue linguisticValue;
+    List<Quantifier> quantifiers;
+    List<LinguisticValue> linguisticValues;
     List<Player> players;
 
-    public YagerCalculator(Quantifier quantifier, LinguisticValue linguisticValue, List<Player> players) {
-        this.quantifier = quantifier;
-        this.linguisticValue = linguisticValue;
+    public YagerCalculator(List<Quantifier> quantifier, List<LinguisticValue> linguisticValue, List<Player> players) {
+        this.quantifiers = quantifier;
+        this.linguisticValues = linguisticValue;
         this.players = players;
     }
 
-    public double calculateYager() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        System.out.println(quantifier.getDescription());
-        System.out.println(linguisticValue.getDescription());
+    public void calculateYager() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        for (Quantifier quantifier : quantifiers) {
+            for (LinguisticValue linguisticValue : linguisticValues) {
+                calculateYagerForEachLinguisticValue(quantifier, linguisticValue);
+            }
+        }
+    }
+
+    private void calculateYagerForEachLinguisticValue(Quantifier quantifier, LinguisticValue linguisticValue) throws InvocationTargetException, IllegalAccessException {
+        System.out.print(quantifier.getDescription() + " ");
+        System.out.print(linguisticValue.getDescription() + " ");
         TriangularFunction triangularFunctionQuantifiers = new TriangularFunction(quantifier.getPoints());
         TriangularFunction triangularFunctionLinguisticValues = new TriangularFunction(linguisticValue.getPoints());
         double cardinalValue = 0.0;
         for (Player player : players) {
-            cardinalValue += getMembershipValue(triangularFunctionLinguisticValues, player);
+            cardinalValue += getMembershipValue(triangularFunctionLinguisticValues, player, linguisticValue);
         }
-        System.out.println(cardinalValue);
-        System.out.println(triangularFunctionQuantifiers.getMembership(cardinalValue));
-        return triangularFunctionQuantifiers.getMembership(cardinalValue);
+        System.out.printf("%.2f", cardinalValue);
+        System.out.print(" ");
+        System.out.printf("%.2f", triangularFunctionQuantifiers.getMembership(cardinalValue));
+        System.out.println();
     }
 
-    private double getMembershipValue(TriangularFunction triangularFunctionLinguisticValues, Player player) throws InvocationTargetException, IllegalAccessException {
-        return triangularFunctionLinguisticValues.getMembership(getValue(player));
+    private double getMembershipValue(TriangularFunction triangularFunctionLinguisticValues, Player player, LinguisticValue linguisticValue) throws InvocationTargetException, IllegalAccessException {
+        return triangularFunctionLinguisticValues.getMembership(getValue(player, linguisticValue));
     }
 
-    private int getValue(Player player) throws InvocationTargetException, IllegalAccessException {
+    private int getValue(Player player, LinguisticValue linguisticValue) throws InvocationTargetException, IllegalAccessException {
         return (int) linguisticValue.getMethod().invoke(player);
     }
 }
--- src/main/java/com/tul/ksr/Main.java
+++ src/main/java/com/tul/ksr/Main.java
@@ -3,6 +3,7 @@ package com.tul.ksr;
 import com.tul.ksr.Fuzzy.Operations.YagerCalculator;
 import com.tul.ksr.Model.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -15,15 +16,25 @@ public class Main {
 //        TriangularFunction trian2 = new TriangularFunction(new ArrayList<>(asList(2.0, 6.0, 10.0)));
 //        OperationManager operation = new OperationManager(asList(trian1, trian2));
 //        System.out.println(operation.getIntersectionMembership(7));
+
+
         DataCreator dataCreator = new DataCreator();
         List<Quantifier> quantifierList = dataCreator.createQuantifiers();
         List<LinguisticValue> linguisticValues = dataCreator.createLinguisticValue();
+
+        List<LinguisticValue> severalLinguisticValues = new ArrayList<>();
+        severalLinguisticValues.add(linguisticValues.get(2));
+        severalLinguisticValues.add(linguisticValues.get(3));
+        severalLinguisticValues.add(linguisticValues.get(4));
+
+        List<Quantifier> severalQuantifiers = new ArrayList<>();
+        severalQuantifiers.add(quantifierList.get(3));
+        severalQuantifiers.add(quantifierList.get(4));
+
+
         List<Player> players = DataParser.createPlayerList();
-        YagerCalculator yagerCalculator = new YagerCalculator(quantifierList.get(4), linguisticValues.get(2), players);
+        YagerCalculator yagerCalculator = new YagerCalculator(severalQuantifiers, severalLinguisticValues, players);
         yagerCalculator.calculateYager();
-        YagerCalculator yagerCalculator1 = new YagerCalculator(quantifierList.get(4), linguisticValues.get(3), players);
-        yagerCalculator1.calculateYager();
-        YagerCalculator yagerCalculator2 = new YagerCalculator(quantifierList.get(4), linguisticValues.get(4), players);
-        yagerCalculator2.calculateYager();
+
     }
 }
